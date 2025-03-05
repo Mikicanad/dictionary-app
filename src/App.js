116ea5cd015d07a79f1e53f93fb604bf8b7187f9
Mikicanad <mmmcf5290@gmail.com>
2025-03-05 14:14:43 -0800

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./miki-logo.png";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer"><small>Coded by Miki.I</small></footer>
+      </div>
     </div>
   );
 }
