7882a0ca987918cc4a078d1b5d6eafe5409a81f4
Mikicanad <mmmcf5290@gmail.com>
2025-03-24 13:48:29 -0700

    added Phonetic.js and Photo.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,17 +1,11 @@
 import React from "react";
+import "./Phonetic.css";
 
-
-export default function Synonyms(props) {
-  if (props.synonyms) {
+export default function Phonetic(props) {
+  if (props.phonetic) {
     return (
-      <div className="Synonyms">
-        <strong>Similar:</strong>
-
-        <ul>
-          {props.synonyms.map(function (synonym, index) {
-            return <li key={index}>{synonym}</li>;
-          })}
-        </ul>
+      <div className="Phonetic">
+        <h2>{props.phonetic}</h2>
       </div>
     );
   } else {
